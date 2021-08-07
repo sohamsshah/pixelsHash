3af51fa8c665d3331ed5d28c581b970070c719a4
sohamsshah <sohamshah456@gmail.com>
2021-08-08 00:18:10 +0530

    refactor(refactor file structure 📃): add eslint and prettier config
    fix #11

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+      node: true,
+    },
+    "ignorePatterns": [ "**/server/*"],
+    parser: "babel-eslint",
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "prettier",
+      "prettier/react",
+    ],
+    parserOptions: {
+      ecmaVersion: "2017",
+      ecmaFeatures: {
+        experimentalObjectRestSpread: true,
+        jsx: true,
+      },
+      sourceType: "module",
+    },
+    plugins: ["babel", "react", "import","react-hooks"],
+    rules: {
+      "import/no-duplicates": "error",
+      "import/no-unresolved": "off",
+      "import/named": "error",
+      "react/no-typos": "error",
+      "react/jsx-no-bind": "off",
+      "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+      "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "array-callback-return": "error",
+      "consistent-return": "error",
+      "babel/no-invalid-this": "error",
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "react/prop-types": "off",
+      "react/no-unescaped-entities":"off"
+    },
+    settings: {
+      react: {
+        pragma: "React",
+        version: "detect",
+        flowVersion: "0.63.1",
+      },
+    },
+  };
--- pages/about.js
+++ pages/about.js
@@ -2,11 +2,11 @@ import Link from 'next/link'
 import React from 'react'
 
 const About = () => {
-    return (
-        <div>
-          <Link href="/">Back</Link>  
-        </div>
-    )
+	return (
+		<div>
+			<Link href="/">Back</Link>
+		</div>
+	)
 }
 
 export default About
