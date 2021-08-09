8a9504be70e530db76d39c37a0a55ed8db2dd1dc
sohamsshah <sohamshah456@gmail.com>
2021-08-10 01:21:29 +0530

    feat(list-grid toggle): add toggle button for list and grid view

---
+++ assets/ListIcon.jsx
@@ -0,0 +1,10 @@
+export function MdiFormatListBulletedSquare(props) {
+	return (
+		<svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+			<path
+				d="M3 4h4v4H3V4m6 1v2h12V5H9m-6 5h4v4H3v-4m6 1v2h12v-2H9m-6 5h4v4H3v-4m6 1v2h12v-2H9"
+				fill="currentColor"
+			></path>
+		</svg>
+	)
+}
